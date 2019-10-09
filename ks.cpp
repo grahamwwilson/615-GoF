6ddf25a9d18c6301e58f6e28996a611849ed504d
graham <Graham.Wilson@cern.ch>
2019-10-09 13:17:57 -0500

    Use ks.h header file

--- ks.cpp
+++ ks.cpp
@@ -3,30 +3,8 @@
 #include <iostream>
 #include <cmath>
 #include <iomanip>
+#include "ks.h"
 using namespace std;
-
-double pks(double z) {
-// cdf of the normalized KS distribution (see Ch. 6.14.12 of NR)
-		if (z < 0.0) throw("bad z in KSdist");
-		if (z == 0.0) return 0.0;
-		if (z < 1.18) {
-			double y = exp(-1.23370055013616983/(z*z));
-			return 2.25675833419102515*sqrt(-log(y))
-				*(y + pow(y,9) + pow(y,25) + pow(y,49));
-		} else {
-			double x = exp(-2.0*z*z);
-			return 1.0 - 2.0*(x - pow(x,4) + pow(x,9));
-		}
-}
-double qks(double z) {
-// complementary cdf of the normalized 
-// KS distribution (see Ch. 6.14.12 of NR)
-		if (z < 0.0) throw("bad z in KSdist");
-		if (z == 0.0) return 1.0;
-		if (z < 1.18) return 1.0-pks(z);
-		double x = exp(-2.0*z*z);
-		return 2.0*(x - pow(x,4) + pow(x,9));
-}
 
 int main(){
 
--- test.cpp
+++ test.cpp
@@ -4,30 +4,7 @@
 #include <cmath>
 #include <TRandom3.h>
 #include <vector>
-//using namespace std;
-
-double pks(double z) {
-// cdf of the normalized KS distribution (see Ch. 6.14.12 of NR)
-		if (z < 0.0) throw("bad z in KSdist");
-		if (z == 0.0) return 0.0;
-		if (z < 1.18) {
-			double y = exp(-1.23370055013616983/(z*z));
-			return 2.25675833419102515*sqrt(-log(y))
-				*(y + pow(y,9) + pow(y,25) + pow(y,49));
-		} else {
-			double x = exp(-2.0*z*z);
-			return 1.0 - 2.0*(x - pow(x,4) + pow(x,9));
-		}
-}
-double qks(double z) {
-// complementary cdf of the normalized 
-// KS distribution (see Ch. 6.14.12 of NR)
-		if (z < 0.0) throw("bad z in KSdist");
-		if (z == 0.0) return 1.0;
-		if (z < 1.18) return 1.0-pks(z);
-		double x = exp(-2.0*z*z);
-		return 2.0*(x - pow(x,4) + pow(x,9));
-}
+#include "ks.h"
 
 int main(){
 
